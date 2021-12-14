0a4739be33a2471d881845101ed724582ea7b8df
Edward Wei <ewei2406@gmail.com>
2021-12-14 16:55:02 -0500

    m

--- test_subgraph.sh
+++ test_subgraph.sh
@@ -1,1 +1,1 @@
-python3 subgraph.py --reg_epochs 100 --ptb_epochs 20 --surrogate_epochs 10 --ptb_rate 2 --protected_label 3 --surrogate_train N --csv trials
+python3 subgraph.py --reg_epochs 100 --ptb_epochs 20 --surrogate_epochs 10 --ptb_rate 1 --protected_label 3 --surrogate_train N --csv trials
